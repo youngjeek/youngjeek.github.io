c51757daf963f7ecf15f3fe4d3bb5917658044d9
youngjeek <youngjeek@gmail.com>
2022-12-14 17:37:11 +0900

    N_React#6.19 persistAtom

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,22 +1,52 @@
-import { IToDo } from './atoms';
+import React from 'react';
+import { IToDo, toDoState, Categories, categoryList } from './atoms';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
-function ToDo({ text, category }: IToDo) {
-  const onClick = (newCategory: IToDo['category']) => {
-    console.log('I wanna to', newCategory);
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const customCategory = useRecoilValue(categoryList);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      // const oldToDo = oldToDos[targetIndex];
+      const newToDo = { text, id, category: name };
+      console.log('i wanna go to', targetIndex, 'with', newToDo);
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
   return (
     <li>
       <span>{text}</span>
-      {category !== 'NOT_YET' && (
-        <button onClick={() => onClick('NOT_YET')}>Not yet</button>
+      {category !== Categories.NOT_YET && (
+        <button name={Categories.NOT_YET + ''} onClick={onClick}>
+          Not yet
+        </button>
       )}
-      {category !== 'ACTIVE' && (
-        <button onClick={() => onClick('ACTIVE')}>Active</button>
+      {category !== Categories.ACTIVE && (
+        <button name={Categories.ACTIVE + ''} onClick={onClick}>
+          Active
+        </button>
       )}
-      {category !== 'DONE' && (
-        <button onClick={() => onClick('DONE')}>Done</button>
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE + ''} onClick={onClick}>
+          Done
+        </button>
       )}
+      {category !== Object.keys(customCategory) &&
+        Object.keys(customCategory).map((newOpt) => (
+          <button name={newOpt} onClick={onClick}>
+            {newOpt}
+          </button>
+        ))}
     </li>
   );
 }
+
 export default ToDo;
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,19 +1,72 @@
-import { useRecoilValue } from 'recoil';
-import { toDoState } from './atoms';
+import React from 'react';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { categoryList, categoryState, toDoSelector, toDoState } from './atoms';
 import CreateToDo from './CreateToDo';
 import ToDo from './ToDo';
 
 function ToDoList() {
   const toDo = useRecoilValue(toDoState);
+  const customCategory = useRecoilValue(categoryList);
+  const [toDos, doing, done, custom] = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) =>
+    setCategory(event.currentTarget.value as any);
+  console.log(toDo);
+  function onClick(newCategory: string) {
+    return toDo
+      .filter((toDo) => toDo.category === newCategory)
+      .map((toDoo) => <ToDo key={toDoo.id} {...toDoo} />);
+  }
 
   return (
     <div>
+      <h1>To-do List</h1>
+      <hr />
+      Choose Status :
+      <select value={category} onInput={onInput}>
+        <option value="NOT_YET">Not yet</option>
+        <option value="ACTIVE">Active</option>
+        <option value="DONE">Done</option>
+        {Object.keys(customCategory).map((newOpt) => (
+          <option value={newOpt}>{newOpt}</option>
+        ))}
+      </select>
       <CreateToDo />
+      <h2>To-do</h2>
       <ul>
-        {toDo.map((toDoo) => (
+        {toDos.map((toDoo) => (
           <ToDo key={toDoo.id} {...toDoo} />
         ))}
       </ul>
+      <hr />
+      <h2>in Progress</h2>
+      <ul>
+        {doing.map((toDoo) => (
+          <ToDo key={toDoo.id} {...toDoo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDoo) => (
+          <ToDo key={toDoo.id} {...toDoo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Custom</h2>
+      <ul>
+        {Object.keys(customCategory).map((toDoo) => (
+          <button key={toDoo} onClick={() => onClick(toDoo)}>
+            {toDoo}
+          </button>
+        ))}
+      </ul>
+      <ul>
+        {custom.map((toDoo) => (
+          <ToDo key={toDoo.id} {...toDoo} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
